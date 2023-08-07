79749a85c9e62b97d4f183613e6e9909417e85b9
Raphael Mazzieri <rquitto@gmail.com>
2023-08-07 14:15:39 -0300

    adicionados arquivos pela primeira vez

---
+++ index.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event){
+    const inputEmail = form.querySelector('.email-field');
+
+    let  errorAlert = form.querySelector('.error-alert');
+
+    event.preventDefault();
+
+    if(inputEmail.value === ''){
+    
+        if(errorAlert.classList.contains('error-alert')){
+            errorAlert.style.visibility = 'visible';
+            inputEmail.style.borderColor = 'hsl(354, 100%, 66%)';
+        }
+
+    }else {
+        errorAlert.style.visibility ='hidden';
+        inputEmail.style.borderColor = 'hsl(109, 80%, 48%)';
+    }
+});
